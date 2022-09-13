ab05c1e6616c8a977c7de08084a90dce30fce577
formegusto <formegusto@gmail.com>
2022-09-13 10:42:28 +0900

    tailwind 배경 파악 완료

--- tenergy-fe/src/App.tsx
+++ tenergy-fe/src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">hello world!</h1>
+      <button className="bg-slate-900 hover:bg-slate-600 text-white px-4 py-2 box-border rounded duration-150 ease-in-out">
+        이게 되낭?
+      </button>
+    </>
+  );
 }
 
 export default App;
