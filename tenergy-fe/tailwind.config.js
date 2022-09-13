197a264145208bd3658b744c8d27eb1e897b61f7
formegusto <formegusto@gmail.com>
2022-09-13 10:45:38 +0900

    우아 폰트도 대넹

--- tenergy-fe/tailwind.config.js
+++ tenergy-fe/tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      spoqa: ["Spoqa Han Sans Neo", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
