1c01bdb818dbe4ae948bae26e94a4b85daf7fdb0
formegusto <formegusto@gmail.com>
2022-09-22 15:50:58 +0900

    admin api proto okay

--- tenergy-be/src/routes/public/GET/index.ts
+++ tenergy-be/src/routes/public/GET/index.ts
@@ -2,6 +2,8 @@ import Express from "express";
 import Distributor from "@libs/Distributor";
 import { adminCheck, loginCheck } from "@mw";
 import { StatusCodes } from "http-status-codes";
+import { APT } from "@models/types";
+import _ from "lodash";
 
 const routes = Express.Router();
 
@@ -16,6 +18,90 @@ routes.get(
     return res.status(StatusCodes.OK).json({
       privatePrice: distributor.apt.compares![0].after.public,
       distributePrices: distributor.distributeGroup,
+    });
+  }
+);
+
+routes.get(
+  "/price",
+  loginCheck,
+  adminCheck,
+  async (req: Express.Request, res: Express.Response) => {
+    const apt = new APT();
+    await apt.addCompares();
+    await apt.addTrades();
+
+    return res.status(StatusCodes.OK).json({
+      apartment: apt.bill,
+      householdPart: apt.householdBill,
+      householdCount: apt.householdCount,
+      tradePart: apt.tradeBill,
+      publicPart: apt.publicBill,
+      priPublicPrice: Math.round(apt.publicBill / apt.householdCount!),
+    });
+  }
+);
+
+routes.get(
+  "/detail",
+  loginCheck,
+  adminCheck,
+  async (req: Express.Request, res: Express.Response) => {
+    const distributor = await Distributor.init();
+    await distributor.build();
+
+    // groupNo
+    const groups = distributor.groups;
+    const groupCount = _.valuesIn(_.countBy(groups));
+    const uniqGroups = _.sortBy(_.uniq(groups));
+
+    // 가격정보
+    const priPrice = distributor.publicPrice! / distributor.apt.householdCount!;
+    const distribute = distributor.distributeGroup;
+    const errs = _.map(distribute, (dist) => dist - priPrice);
+
+    // 패턴, 평균사용량 정보
+    const apt = distributor.apt;
+    await apt.addHouseholds();
+    _.forEach(apt.households, (household, idx) => {
+      household.group = groups![idx];
+    });
+    const groupPats = _.map(uniqGroups, (group) =>
+      _.map(
+        _.filter(apt.households, (household) => household.group === group),
+        ({ pat }) => pat
+      )
+    );
+    const meanUsages = _.map(groupPats, (pats) =>
+      Math.round(_.mean(_.map(pats, _.sum)))
+    );
+    const meanPats = _.map(groupPats, (pats) =>
+      _.map(_.chunk(_.map(_.zip.apply(null, pats), _.mean), 24), _.sum)
+    );
+
+    const contributions = distributor.contributions;
+
+    const colNames = [
+      "groupNo",
+      "count",
+      "congtribution",
+      "meanUsage",
+      "price",
+      "err",
+    ];
+    const tableDatas = _.zip(
+      uniqGroups,
+      groupCount,
+      contributions,
+      meanUsages,
+      distribute,
+      errs
+    );
+
+    return res.status(StatusCodes.OK).json({
+      groupCount,
+      groupPattern: meanPats,
+      table: _.map(tableDatas, (tableData) => _.zipObject(colNames, tableData)),
     });
   }
 );
--- tenergy-be/src/routes/trade/GET/index.ts
+++ tenergy-be/src/routes/trade/GET/index.ts
@@ -1,23 +1,90 @@
+import Distributor from "@libs/Distributor";
 import { APT } from "@models/types";
+import { adminCheck, loginCheck } from "@mw";
 import Express from "express";
 import { StatusCodes } from "http-status-codes";
 import _ from "lodash";
 
 const routes = Express.Router();
 
-routes.get("/", async (req: Express.Request, res: Express.Response) => {
-  const apt = new APT();
+routes.get(
+  "/",
+  loginCheck,
+  adminCheck,
+  async (req: Express.Request, res: Express.Response) => {
+    const apt = new APT();
 
-  await apt.addTrades();
-  const trades = apt.trades!;
+    await apt.addTrades();
+    const trades = apt.trades!;
 
-  return res.status(StatusCodes.OK).json({
-    // 거래 사용량
-    usage: _.sumBy(trades, ({ quantity }) => quantity),
-    count: trades.length,
-    meanUnit: _.meanBy(trades, ({ quantity }) => quantity),
-    meanPrice: _.meanBy(trades, ({ price }) => price),
-  });
-});
+    return res.status(StatusCodes.OK).json({
+      // 거래 사용량
+      usage: _.sumBy(trades, ({ quantity }) => quantity),
+      count: trades.length,
+      meanUnit: _.meanBy(trades, ({ quantity }) => quantity),
+      meanPrice: _.meanBy(trades, ({ price }) => price),
+    });
+  }
+);
+
+routes.get(
+  "/detail",
+  loginCheck,
+  adminCheck,
+  async (req: Express.Request, res: Express.Response) => {
+    const apt = new APT();
+
+    await apt.addTrades();
+    await apt.addCompares();
+    const trades = apt.trades!;
+    const prices = _.map(trades, ({ price }) => price);
+    const groupByNameTrade = _.groupBy(trades, ({ name }) => name);
+
+    const groupingTrades = _.values(groupByNameTrade);
+    const groupTotals = _.map(groupingTrades, (groupTrade) =>
+      _.sumBy(groupTrade, ({ quantity }) => quantity)
+    );
+
+    return res.status(StatusCodes.OK).json({
+      price: {
+        value: _.sum(prices),
+        mean: _.sum(prices) / apt.householdCount!,
+        min: _.min(prices),
+        max: _.max(prices),
+      },
+      usage: {
+        value: _.sum(groupTotals),
+        mean: _.mean(groupTotals),
+        min: _.min(groupTotals),
+        max: _.max(groupTotals),
+      },
+    });
+  }
+);
+
+routes.get(
+  "/compare",
+  loginCheck,
+  adminCheck,
+  async (req: Express.Request, res: Express.Response) => {
+    const apt = new APT();
+    const distributor = await Distributor.init();
+
+    await apt.addCompares();
+    await distributor.build();
+
+    // compares랑 순서같음
+    // public 가격 재 주입
+    const distributes = distributor.distribute;
+    const compares = _.map(apt.compares!, (compare, idx) => {
+      compare.after.public = distributes[idx];
+      compare.err.public = compare.after.public - compare.before.public;
+
+      return compare;
+    });
+
+    return res.status(StatusCodes.OK).json(compares);
+  }
+);
 
 export default routes;
